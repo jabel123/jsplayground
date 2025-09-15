0532611fe5bd1b87a652ec0faf2f007a42e46654
주현태 <jabel123@naver.com>
2025-09-15 21:36:24 +0900

    📝 250915 algorithm

---
+++ algorithm/2025/09/250915.js
@@ -0,0 +1,10 @@
+// 8 kyu Training JS #5: Basic data types--Object
+// my solution
+const animal = (obj) => "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs.";
+
+
+/**
+ * best solution
+ * `を使って並ぶ方が見やすい感がある
+ */
+// cosnt animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} lengs.`;
